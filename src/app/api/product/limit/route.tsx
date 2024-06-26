6aa96d5a2020f4c351f66afa61c5d98e5971792f
Radz Santillan <116485181+radz22@users.noreply.github.com>
2024-06-26 20:44:11 +0800

    computer change

---
+++ src/app/api/product/limit/route.tsx
@@ -0,0 +1,23 @@
+"use server";
+
+import { NextResponse, NextRequest } from "next/server";
+import inventoryModel from "../../../../../MongooseSchema/InventorySchema";
+import dbConnection from "../../../../../dbsetup/mongodbsetup";
+
+export async function POST(request: NextRequest) {
+  await dbConnection();
+  try {
+    const { limit, offset } = await request.json();
+    const data = await inventoryModel
+      .find()
+      .skip(parseInt(offset))
+      .limit(parseInt(limit));
+
+    return NextResponse.json(data, { status: 200 });
+  } catch {
+    return NextResponse.json(
+      { message: "Server Error / Backend Error" },
+      { status: 400 }
+    );
+  }
+}
